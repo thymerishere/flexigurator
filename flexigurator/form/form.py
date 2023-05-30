6a8952a0d136abba6aea39fb1fcb92b4cb6e0e03
Thomas Bos <tbos@infiniot.nl>
2023-05-30 10:46:18 +0200

    Added form tests for non-app methods

--- flexigurator/form/form.py
+++ flexigurator/form/form.py
@@ -2,7 +2,7 @@ import json
 import os
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Type
+from typing import Any, Type
 
 import mmh3
 import yaml
@@ -34,18 +34,34 @@ def _load_config_templates(path: Path) -> list[ConfigTemplate]:
     return templates
 
 
-def _load_yaml(path: Path) -> str:
+def _load_yaml(path: Path) -> dict[str, Any]:
     with open(path, "r") as yaml_file:
         yaml_file_contents = yaml_file.read()
 
     yaml_object = yaml.safe_load(yaml_file_contents)
 
-    return yaml_object if yaml_object else "{}"
+    return yaml_object if yaml_object else {}
+
+
+def _config_form_start_vals(uid: str, config_templates: list[ConfigTemplate]) -> str:
+    path = [template for template in config_templates if template.uid == uid][0].path
+    return json.dumps(_load_yaml(path))
 
 
 def _write_to_file(file_path: Path, contents: str) -> None:
     with open(file_path, "w") as file:
         file.write(contents)
+
+
+def _save_config_form_output(json_: dict[str, Any], file_name: str, save_path: Path):
+    if json_ == {}:
+        yaml_str = ""
+    else:
+        yaml_str = yaml.dump(json_)
+
+    yaml_file_path = save_path.joinpath(Path(file_name + ".yaml"))
+    yaml_file_path.parent.mkdir(parents=True, exist_ok=True)
+    _write_to_file(yaml_file_path, yaml_str)
 
 
 def ConfigForm(
@@ -57,6 +73,7 @@ def ConfigForm(
     app = FastAPI()
     templates = Jinja2Templates(directory=jinja_templates_path)
     config_templates = _load_config_templates(config_templates_path)
+    schema = config.schema()
 
     @app.get("/", response_model=None)
     async def root(request: Request) -> Response:
@@ -68,32 +85,19 @@ def ConfigForm(
 
     @app.get("/config_template/{uid}", response_model=None)
     async def config_form(request: Request, uid: str) -> Response:
-        path = [template for template in config_templates if template.uid == uid][0].path
-        start_val = json.dumps(_load_yaml(path))
-
-        config_schema_json = config.schema_json()
-
         return templates.TemplateResponse(
             "config_form.html",
             {
                 "request": request,
-                "schema_json": config_schema_json,
-                "start_val": start_val,
+                "schema_json": schema,
+                "start_val": _config_form_start_vals(uid, config_templates),
             },
         )
 
     @app.post("/config_json/{file_name}", response_model=None)
     async def _config_json(request: Request, file_name: str) -> dict[str, str]:
-        json_str = await request.json()
-
-        if json_str == {}:
-            yaml_str = ""
-        else:
-            yaml_str = yaml.dump(json_str)
-
-        yaml_file_name = file_name + ".yaml"
-
-        _write_to_file(config_save_path.joinpath(yaml_file_name), yaml_str)
-        return {"message": f"Parsed config json {json_str}!"}
+        json_ = await request.json()
+        _save_config_form_output(json_, file_name, config_save_path)
+        return {"message": f"Parsed config json {json_}!"}
 
     return app
---
+++ tests/form/test_form.py
@@ -0,0 +1,149 @@
+from pathlib import Path
+import tempfile
+from unittest.mock import MagicMock
+
+from flexigurator.form.form import (
+    ConfigTemplate,
+    _config_form_start_vals,
+    _load_config_templates,
+    _load_yaml,
+    _save_config_form_output,
+)
+from pytest_mock import MockerFixture
+
+
+def test_load_config_templates(mocker: MockerFixture):
+    with tempfile.TemporaryDirectory(
+        dir="./",
+    ) as folder_name:
+        folder_path = Path(folder_name)
+
+        files = [
+            "config_one.yaml",
+            "config_two.yaml",
+            "nested/config_three.yaml",
+            "nested/also_nested/config_four.yaml",
+        ]
+
+        file_paths = [folder_path.joinpath(Path(file_name)) for file_name in files]
+
+        for file_path in file_paths:
+            file_path.parent.mkdir(exist_ok=True, parents=True)
+            open(file_path, "w+")
+
+        hash_mock = MagicMock()
+        hash_mock.hash.side_effect = ["1", "2", "3", "4"]
+        mocker.patch("flexigurator.form.form.mmh3", hash_mock)
+
+        expected = [
+            ConfigTemplate("1", "config_one", folder_path.joinpath(Path("config_one.yaml"))),
+            ConfigTemplate("2", "config_two", folder_path.joinpath(Path("config_two.yaml"))),
+            # Glob returns in alphabetical order
+            ConfigTemplate(
+                "4",
+                "nested/also_nested/config_four",
+                folder_path.joinpath(Path("nested/also_nested/config_four.yaml")),
+            ),
+            ConfigTemplate(
+                "3", "nested/config_three", folder_path.joinpath(Path("nested/config_three.yaml"))
+            ),
+        ]
+
+        actual = _load_config_templates(folder_path)
+
+    assert actual == expected
+
+
+def test_load_yaml():
+    yaml_str = """
+    a: 5
+    b: 6
+    c: test
+    """
+
+    expected = {"a": 5, "b": 6, "c": "test"}
+
+    with tempfile.NamedTemporaryFile(mode="w", suffix=".yaml") as temp_yaml:
+        temp_yaml.write(yaml_str)
+        temp_yaml.flush()
+        file_path = Path(temp_yaml.name)
+
+        actual = _load_yaml(file_path)
+
+    assert actual == expected
+
+
+def test_load_yaml_empty():
+    expected = {}
+
+    with tempfile.NamedTemporaryFile(mode="w", suffix=".yaml") as temp_yaml:
+        file_path = Path(temp_yaml.name)
+        actual = _load_yaml(file_path)
+
+    assert actual == expected
+
+
+def test_save_config_form_output():
+    json_ = dict(a=1, b=2, c="test", d=dict(some_int=42))
+    file_name = "test"
+
+    expected = ["a: 1\n", "b: 2\n", "c: test\n", "d:\n", "  some_int: 42\n"]
+
+    with tempfile.TemporaryDirectory(
+        dir="./",
+    ) as folder_name:
+        folder_path = Path(folder_name)
+        _save_config_form_output(json_, file_name, folder_path)
+
+        with open(folder_path.joinpath(Path(file_name + ".yaml")), "r") as actual_file:
+            actual = actual_file.readlines()
+
+    assert actual == expected
+
+
+def test_save_config_form_output_json_empty():
+    json_ = {}
+    file_name = "test"
+
+    expected = []
+
+    with tempfile.TemporaryDirectory(
+        dir="./",
+    ) as folder_name:
+        folder_path = Path(folder_name)
+        _save_config_form_output(json_, file_name, folder_path)
+
+        with open(folder_path.joinpath(Path(file_name + ".yaml")), "r") as actual_file:
+            actual = actual_file.readlines()
+
+    assert actual == expected
+
+
+def test_config_form_start_vals():
+    json_ = dict(a=1, b=2, c="test", d=dict(some_int=42))
+    expected = '{"a": 1, "b": 2, "c": "test", "d": {"some_int": 42}}'
+
+    with tempfile.TemporaryDirectory(
+        dir="./",
+    ) as folder_name:
+        folder_path = Path(folder_name)
+
+        templates = [
+            ConfigTemplate("1", "config_one", folder_path.joinpath(Path("config_one.yaml"))),
+            ConfigTemplate("2", "config_two", folder_path.joinpath(Path("config_two.yaml"))),
+            # Glob returns in alphabetical order
+            ConfigTemplate(
+                "4",
+                "nested/also_nested/config_four",
+                folder_path.joinpath(Path("nested/also_nested/config_four.yaml")),
+            ),
+            ConfigTemplate(
+                "3", "nested/config_three", folder_path.joinpath(Path("nested/config_three.yaml"))
+            ),
+        ]
+
+        _save_config_form_output(json_, "nested/config_three", folder_path)
+
+        actual = _config_form_start_vals("3", templates)
+
+    assert actual == expected
