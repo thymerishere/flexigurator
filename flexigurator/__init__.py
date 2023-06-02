a7fc9f06b997b74cfd5de31ef6fae1375758e839
Thomas Bos <tbos@infiniot.nl>
2023-06-02 15:36:57 +0200

    Pipeline

--- flexigurator/__init__.py
+++ flexigurator/__init__.py
@@ -1,3 +1,3 @@
-from flexigurator.placeholder import NotConfiguredError, placeholder
 from flexigurator.config_patch import patch_config
 from flexigurator.form.form import ConfigForm
+from flexigurator.placeholder import NotConfiguredError, placeholder
