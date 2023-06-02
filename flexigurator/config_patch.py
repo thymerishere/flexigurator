e374e63f918ae20906da092a8330ff52910cc5e4
Thomas Bos <tbos@infiniot.nl>
2023-06-02 14:02:42 +0200

    Add config patcher

---
+++ flexigurator/config_patch.py
@@ -0,0 +1,43 @@
+from contextlib import contextmanager
+from typing import Any, Generator, Iterator, Type
+
+from confz import ConfZ, ConfZDataSource, ConfZSource
+
+
+@contextmanager
+def patch_config(
+    config_class: Type[ConfZ], data: dict[str, Any] | ConfZSource | list[ConfZSource]
+) -> Iterator[None]:
+    """Patch a config class with additional sources.
+
+    This context manager temporarily updates the given config class with the provided data sources.
+    This is useful for testing where a specific set of configuration values needs to be changed for
+    a single test instance. Importantly, the provided configuration does not need to be complete,
+    as long as the original configuration sources are.
+
+    Args:
+        config_class (Type[ConfZ]): The ConfZ config class
+        data (dict[str, Any] | ConfZSource | list[ConfZSource]): A source of configuration in the
+            form of a dictionary or one or more ConfZ sources
+
+    Yields:
+        None: This context manager does not yield
+
+    """
+    if isinstance(data, dict):
+        data = ConfZDataSource(data)
+
+    if not isinstance(data, list):
+        data = [data]
+
+    patched_sources = config_class.CONFIG_SOURCES
+
+    if patched_sources is None:
+        patched_sources = []
+    elif not isinstance(patched_sources, list):
+        patched_sources = [patched_sources]
+
+    patched_sources += data
+
+    with config_class.change_config_sources(patched_sources):
+        yield
